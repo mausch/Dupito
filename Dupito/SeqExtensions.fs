2aa5d995bda5578fa5adb84c6f00c08388d2c5a2
mausch <mauricioscheffer@gmail.com>
2010-04-03 20:20:51 -0300

    had the parameters backwards

--- Dupito/SeqExtensions.fs
+++ Dupito/SeqExtensions.fs
@@ -3,6 +3,6 @@
 let hasElem e s = 
     s |> Seq.exists (fun i -> i = e)
 
-let except s1 s2 = 
+let except s2 s1 = 
     let notHasElem e = (hasElem e) >> not
     s1 |> Seq.filter (fun i -> s2 |> notHasElem i)
