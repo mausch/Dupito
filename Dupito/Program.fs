4bc8d79164a5dfde6ec1e9bf127330a11228e163
mausch <mauricioscheffer@gmail.com>
2010-09-29 22:01:06 -0300

    cleanup, comments

--- Dupito/Program.fs
+++ Dupito/Program.fs
@@ -23,15 +23,22 @@ let createConn() =
     conn.Open()
     conn :> IDbConnection
 
+/// Connection manager
 let cmgr = Sql.withNewConnection createConn
 
+// partial application of common functions
+let execReader x = Sql.execReader cmgr x
+let execReaderf x = Sql.execReaderF cmgr x
+let execNonQuery x = Sql.execNonQuery cmgr x
+let execNonQueryf x = Sql.execNonQueryF cmgr x
+let P = Sql.Parameter.make
+
 let createDB() =
-    let exec sql = Sql.execNonQuery cmgr sql [] |> ignore
+    let exec sql = execNonQuery sql [] |> ignore
     FbConnection.CreateDatabase connectionString
     exec "create table filehash (filepath varchar(1000), hash varchar(100))"
     for key in ["filepath"; "hash"] do
-        let sql = sprintf "create index IX_%s on filehash(%s)" key key
-        exec sql
+        exec (sprintf "create index IX_%s on filehash(%s)" key key)
 
 let setupDB () = 
     if not (File.Exists dbFilename) then
@@ -47,11 +54,13 @@ let help () =
     printfn "dd: deletes duplicate files automatically"
     0
 
+/// Hash a file
 let hashFile f = 
     use fs = new FileStream(f, FileMode.Open)
     use hashFunction = new SHA512Managed()
     hashFunction.ComputeHash fs |> Convert.ToBase64String
 
+/// Hash a stream (async)
 let hashAsync bufferSize (hashFunction: HashAlgorithm) (stream: Stream) progressReport =
     let rec hashBlock currentBlock count = async {
         progressReport stream.Position
@@ -70,7 +79,9 @@ let hashAsync bufferSize (hashFunction: HashAlgorithm) (stream: Stream) progress
         return hashFunction.Hash |> Convert.ToBase64String
     }
 
+
 let consolelock = obj()
+/// print to console, locking it to avoid parallel overlapping
 let lprintfn t = 
     let flush (a: string) = 
         System.Console.WriteLine(a)
@@ -78,6 +89,7 @@ let lprintfn t =
         System.Console.Out.Flush()
     lock consolelock (fun () -> Printf.kprintf flush t)
 
+/// Hash a file async
 let hashFileAsync f =
     let bufferSize = 65536
     async {
@@ -92,7 +104,11 @@ type FileHash = {
     Hash: string
     FilePath: string
 }        
+
+let getHash (h: FileHash) = h.Hash
+let getFilepath (h: FileHash) = h.FilePath
     
+/// Hash a file and save it to database
 let indexFile (save : FileHash -> unit) f = 
     printfn "Indexing file %A" f
     try
@@ -100,6 +116,7 @@ let indexFile (save : FileHash -> unit) f =
         save {Hash = hash; FilePath = f}
     with e -> ()
 
+/// Hash a file and save it to database (async)
 let indexFileAsync (save: FileHash -> unit) f =
     async {
         lprintfn "Indexing file %A" f
@@ -112,14 +129,13 @@ let indexFileAsync (save: FileHash -> unit) f =
             ()
     }
 
-
 let add (fileHashEnumerate : unit -> FileHash seq) (fileHashSave : FileHash -> unit) =
     let allFiles = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories)
-    let filesInDb = fileHashEnumerate() |> Seq.map (fun h -> h.FilePath) |> Seq.toList
+    let filesInDb = fileHashEnumerate() |> Seq.map getFilepath |> Seq.toList
     let filesToInsert = allFiles |> Seq.except filesInDb
-    filesToInsert |> Seq.map (indexFileAsync fileHashSave)
-    |> Async.Parallel 
-    |> Async.RunSynchronously
+    filesToInsert 
+    |> Seq.map (indexFileAsync fileHashSave)
+    |> Async.Parallel |> Async.RunSynchronously
     |> ignore
     0
 
@@ -129,45 +145,44 @@ let cleanup (fileHashEnumerate : unit -> FileHash seq) delete =
     |> Seq.iter delete
     0
 
+/// Maps raw data record to FileHash
 let asFileHash = Sql.asRecord<FileHash>
 
-let comparePairs (x1,y1) (x2,y2) = 
-    (x1 = x2 && y1 = y2) || (x1 = y2 && y1 = x2)
+/// Compares pairs disregarding order
+let comparePairs (x1,y1) (x2,y2) = (x1 = x2 && y1 = y2) || (x1 = y2 && y1 = x2)
 
-let applyToPair f (x,y) =
-    (f x, f y)
+let applyToPair f (x,y) = (f x, f y)
 
-let getHash (h: FileHash) = h.Hash
-let getFilepath (h: FileHash) = h.FilePath
 let getHashes = applyToPair getHash
 let getFiledate (h: FileHash) = (FileInfo(h.FilePath)).LastWriteTime
 
+/// Gets all dupes in database
 let getDupes () =
     let fields = Sql.recordFieldsAlias typeof<FileHash>
     let sql = sprintf "select %s,%s from filehash a join filehash b on a.hash = b.hash where a.filepath <> b.filepath" (fields "a") (fields "b")
-    Sql.execReader cmgr sql []
+    execReader sql []
     |> Sql.map (fun r -> asFileHash "a" r, asFileHash "b" r)
     |> Seq.distinctWith (fun x y -> comparePairs (getHashes x) (getHashes y))
     |> Seq.groupBy (fun (x,_) -> x.Hash)
     |> Seq.map (fun (_,y) -> y |> Seq.map fst |> Seq.distinctBy getFilepath |> Seq.sortBy getFiledate)
 
+/// Prints all dupes in database
 let printList () =
     getDupes()
     |> Seq.iter (fun f -> printfn "dupes:\n%s\n" (System.String.Join("\n", f |> Seq.map getFilepath |> Seq.toArray)))
     0
 
 let findAll () =
-    Sql.execReader cmgr "select * from filehash" [] |> Sql.map (asFileHash "")
+    execReader "select * from filehash" [] |> Sql.map (asFileHash "")
 
 let save (f: FileHash) =
-    Sql.execNonQuery cmgr "insert into filehash (hash, filepath) values (@h, @p)"
-        (Sql.parameters ["@h",box f.Hash;"@p",box f.FilePath]) |> ignore
+    execNonQuery "insert into filehash (hash, filepath) values (@h, @p)"
+        [P("@h", f.Hash); P("@p", f.FilePath)] |> ignore
 
 let deleteByPath f =
-    Sql.execNonQueryF cmgr "delete from filehash where filepath = %s" f |> ignore
+    execNonQueryf "delete from filehash where filepath = %s" f |> ignore
 
 let delete (f: FileHash) = deleteByPath f.FilePath
-
 
 let rehash () =
     failwith "not implemented"
@@ -179,7 +194,7 @@ let deleteInteractively () =
 
 let deleteWithoutAsking () =
     getDupes()
-    |> Seq.collect (fun x -> x |> Seq.skip 1)
+    |> Seq.collect (Seq.skip 1)
     |> Seq.map (fun x -> x.FilePath)
     |> Seq.iter ([printfn "deleting %s"; File.Delete; deleteByPath] |> Seq.iterf)
     0
